267bfebee9bbdf90ee4d08990da220ca1647e6a8
sonicxml <sonicxml@gmail.com>
2012-02-08 19:57:37 -0500

    Asynchronous I/O latency to a solid-state disk greatly increased
    between the 2.6.32 and 3.0 kernels. By removing the plug from
    do_io_submit(), we observed a 34% improvement in the I/O latency.
    
    Unfortunately, at this level, we don't know if the request is to
    a rotating disk or not.
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Cc: linux-aio@kvack.org
    Cc: Chris Mason <chris.mason@oracle.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jeff Moyer <jmoyer@redhat.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1620,7 +1620,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1637,8 +1636,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
-	blk_start_plug(&plug);
-
+	
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
 	 * successfully submitted?
@@ -1661,7 +1659,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
