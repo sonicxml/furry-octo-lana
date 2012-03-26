0fce4df3cf2fec894a106e9f36c8ae15a061c348
sonicxml <sonicxml@gmail.com>
2012-03-26 00:16:05 -0400

    power off wlan interface when wifi disabled to save battery - franco

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -2341,7 +2341,6 @@ dhd_stop(struct net_device *net)
 
 	/* Stop the protocol module */
 	dhd_prot_stop(&dhd->pub);
-
 	OLD_MOD_DEC_USE_COUNT;
 exit:
 #if defined(WL_CFG80211)
@@ -2350,7 +2349,6 @@ exit:
 #endif
 	dhd->pub.rxcnt_timeout = 0;
 	dhd->pub.txcnt_timeout = 0;
-
 	DHD_OS_WAKE_UNLOCK(&dhd->pub);
 	return 0;
 }
